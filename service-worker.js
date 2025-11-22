26b322e3bb78bfb4bbdfca429738ea77ab0e5266
Kendrix Brosas <brosaskndrx05@gmail.com>
2025-11-22 17:12:56 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,6 @@ var filesToCache = [
   "/images/appicon-128x128.png",
   "/images/appicon-256x256.png",
   "/images/appicon-512x512.png",
-  "/images/appicon-1024x1024.png",
   "/fallback.html",
 ];
 
